d02f99752148b52042efd74d51ad633a97a8f3de
张为杰 <zhangwj0520@hotmail.com>
2025-08-20 23:06:33 +0800

    sort

--- src/configs/stylistic.ts
+++ src/configs/stylistic.ts
@@ -64,6 +64,7 @@ export async function stylistic(
 
         "style/generator-star-spacing": ["error", { after: true, before: false }],
         "style/jsx-quotes": ["error", "prefer-double"],
+        "style/jsx-sort-props": ["error", { ignoreCase: true, shorthandFirst: true }],
         "style/quotes": ["error", "double", { allowTemplateLiterals: true }],
         "style/yield-star-spacing": ["error", { after: true, before: false }],
 
