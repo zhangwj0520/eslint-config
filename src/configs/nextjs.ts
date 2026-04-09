59e30935bb77fe378e71b5f13b743f957013f193
张为杰 <zhangwj0520@hotmail.com>
2026-04-09 20:15:18 +0800

    0.1.13

--- src/configs/nextjs.ts
+++ src/configs/nextjs.ts
@@ -27,7 +27,7 @@ export async function nextjs(
   function getRules(name: keyof typeof pluginNextJS.configs): Record<string, any> {
     const rules = pluginNextJS.configs?.[name]?.rules;
     if (!rules)
-      throw new Error(`[@antfu/eslint-config] Failed to find config ${name} in @next/eslint-plugin-next`);
+      throw new Error(`[@zhangwj0520/eslint-config] Failed to find config ${name} in @next/eslint-plugin-next`);
     return normalizeRules(rules);
   }
 
